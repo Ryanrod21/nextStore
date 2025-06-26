560071a6ebb75de91de5f92bd207554460c7dd06
Ryan Rodriguez <ryancode2321@gmail.com>
2025-06-25 22:34:53 -0500

    third commit

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -4,9 +4,15 @@ import { getProduct } from '@/api/storeapi';
 import { useParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import './item.css';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { useRef } from 'react';
 
 function ProductPage() {
   const [product, setProduct] = useState(null);
+  const slideRef = useRef(null);
+
   const params = useParams();
   const id = params.id;
 
@@ -26,13 +32,57 @@ function ProductPage() {
     }
   }, [id]);
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  };
+
   if (!product) return <p>Loading...</p>;
 
   return (
     <div className="item-page">
       <h1>{product.title}</h1>
       <p>Rating: {product.rating} / 5</p>
-      <img src={product.thumbnail} alt={product.title} />
+      <div className="product-img-container">
+        <div>
+          {product.images.map((img, index) => (
+            <div key={index}>
+              <img
+                src={img}
+                style={{ width: '150px', height: '150px' }}
+                onClick={() => slideRef.current?.slickGoTo(index)}
+                className="img-side"
+              />
+            </div>
+          ))}
+        </div>
+        <div style={{ maxWidth: '400px', margin: '0 auto' }}>
+          {product.images.length > 1 ? (
+            <Slider ref={slideRef} {...settings}>
+              {product.images.map((src, index) => (
+                <div key={index}>
+                  <img
+                    src={src}
+                    alt={`Image ${index + 1}`}
+                    style={{ width: '100%', display: 'block' }}
+                  />
+                </div>
+              ))}
+            </Slider>
+          ) : (
+            <img
+              src={product.images[0]}
+              alt="Single product image"
+              style={{ width: '100%', display: 'block' }}
+            />
+          )}
+        </div>
+      </div>
+
       <p>Description: {product.description}</p>
       <p>${product.price}</p>
       <p>{product.shippingInformation}</p>
